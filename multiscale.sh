e122809deccf192a0861beafeb9c1c5c351f88be
Sudipta Basak <basaks@gmail.com>
2018-01-12 09:13:29 +1100

    also remove `scale` `.hdr`s

--- multiscale.sh
+++ multiscale.sh
@@ -14,7 +14,7 @@ whitebox_tools -r=MaxElevationDeviation -v --dem=$PWD/$1.flt \
     --min_scale=3 --max_scale=99 --step=1
 
 # remove the unused _scale file
-rm $PWD/${flt}_scale1.flt
+rm $PWD/${flt}_scale1.*
 
 whitebox_tools -r=MaxElevationDeviation -v --dem=$PWD/${flt}.flt \
     -out_mag=$PWD/${flt}_mag2.flt \
@@ -22,7 +22,7 @@ whitebox_tools -r=MaxElevationDeviation -v --dem=$PWD/${flt}.flt \
     --min_scale=100 --max_scale=795 --step=5
 
 # remove the unused _scale file
-rm $PWD/${flt}_scale2.flt
+rm $PWD/${flt}_scale2.*
 
 whitebox_tools -r=MaxElevationDeviation -v --dem=$PWD/${flt}.flt \
     -out_mag=$PWD/${flt}_mag3.flt \
@@ -30,7 +30,7 @@ whitebox_tools -r=MaxElevationDeviation -v --dem=$PWD/${flt}.flt \
     --min_scale=800 --max_scale=1800 --step=10
 
 # remove the unused _scale file
-rm $PWD/${flt}_scale3.flt
+rm $PWD/${flt}_scale3.*
 
 #  MultiscaleTopographicPositionImage is not working on ubuntu
 #whitebox_tools -r=MultiscaleTopographicPositionImage -v \
